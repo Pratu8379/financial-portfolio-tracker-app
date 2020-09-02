eecaf5e8ea2827a0b8054034efd6a13cad862b3e
Pratu8379 <60421793+Pratu8379@users.noreply.github.com>
2020-09-02 12:23:27 +0530

    files deletd

--- my-last1/src/Component/Horsep/Horsep.jsx
+++
@@ -1,7 +0,0 @@
-import React from'react';
-const Horsep =()=>{
-    return(
-        <div style={{ backgroundColor:'black',width:'80%',height:'2px' ,marginTop:'20px',marginBottom:'20px', marginLeft:'150px'}}></div>
-    )
-}
-export default Horsep
